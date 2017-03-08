a7c8985db3213d9fd081227521c9716cba40bbb9
JackMarx <seriousfools@gmail.com>
2017-03-07 19:14:16 -0600

    initial commit

---
+++ lib/jn_hand_dryer.rb
@@ -0,0 +1,9 @@
+require "jn_hand_dryer/version"
+
+module JnHandDryer
+  class Dryer
+    def turn_on
+      `say ooooooooooooooooooooooooooooooooooooooooooooooo oooooooooooooooooo   ooooooooooo`
+    end
+  end
+end
---
+++ lib/jn_hand_dryer/version.rb
@@ -0,0 +1,3 @@
+module JnHandDryer
+  VERSION = "0.1.0"
+end
